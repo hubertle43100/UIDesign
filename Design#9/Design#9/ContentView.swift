8a9c73bd6a90579811178d6cf22f8b4b4b40f6c6
RevealSquid <hubertle43100@gmail.com>
2022-01-13 17:32:13 -0800

    added textfield search bar that has no fuction

--- Design#9/Design#9/ContentView.swift
+++ Design#9/Design#9/ContentView.swift
@@ -8,83 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Binding var text: String
- 
-    @State private var isEditing = false
+    @State private var username: String = ""
     var body: some View {
-        HStack {
-         
-                    TextField("Search ...", text: $text)
-                        .padding(7)
-                        .padding(.horizontal, 25)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                        .overlay(
-                            HStack {
-                                Image(systemName: "magnifyingglass")
-                                    .foregroundColor(.gray)
-                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                                    .padding(.leading, 8)
-                         
-                                if isEditing {
-                                    Button(action: {
-                                        self.text = ""
-                                    }) {
-                                        Image(systemName: "multiply.circle.fill")
-                                            .foregroundColor(.gray)
-                                            .padding(.trailing, 8)
-                                    }
-                                }
-                            }
-                        )
-
-                        .padding(.horizontal, 10)
-                        .onTapGesture {
-                            self.isEditing = true
-                        }
-         
-                    if isEditing {
-                        Button(action: {
-                            self.isEditing = false
-                            self.text = ""
-         
-                        }) {
-                            Text("Cancel")
-                        }
-                        .padding(.trailing, 10)
-                        .transition(.move(edge: .trailing))
-                        .animation(.default)
-                    }
+        Group {
+            VStack {
+                HStack {
+                    Image("1574122306")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(50)
+                    Spacer()
+                    Image("location_on")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Text("Country in North America")
+                    Spacer()
+                    Image(systemName: "bell")
                 }
+                HStack {
+                    Text("Morning best food")
+                        .font(.system(size: 30).bold())
+                    Spacer()
+                }
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .padding()
+                        VStack {
+                            TextField("Search for drinks ...", text: $username)
+                        }
+                        Spacer()
+                    }.frame(width: 360, height: 40)
+                        .background(Color(#colorLiteral(red: 0.9762765765, green: 0.9762766957, blue: 0.9762765765, alpha: 1)))
+                        .cornerRadius(40)
+                    ZStack {
+                        Image("menu 2")
+                            .resizable()
+                        .frame(width: 20, height: 20)
+                    }.frame(width: 40, height: 40)
+                        .background(Color(#colorLiteral(red: 0.7387881875, green: 0.5716782212, blue: 0.3622662723, alpha: 1)))
+                        .cornerRadius(40)
+                }
+                HStack {
+                    Text("Categories")
+                        .font(.system(size: 20).bold())
+                    Spacer()
+                }
+                
             }
+        }.frame(width: 400)
+    }
 }
-//        Group {
-//            VStack {
-//                HStack {
-//                    Image("1574122306")
-//                        .resizable()
-//                        .frame(width: 50, height: 50)
-//                        .cornerRadius(50)
-//                    Spacer()
-//                    Image("location_on")
-//                        .resizable()
-//                        .frame(width: 20, height: 20)
-//                    Text("Country in North America")
-//                    Spacer()
-//                    Image(systemName: "bell")
-//                }
-//                HStack {
-//                    Text("Morning best food")
-//                        .font(.system(size: 30).bold())
-//                    Spacer()
-//                }
-//            }
-//        }.frame(width: 400)
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(text: .constant(""))
+        ContentView()
     }
 }
